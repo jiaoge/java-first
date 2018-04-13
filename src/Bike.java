794c8d41be64e59c0a7aabb62b5bb652b6be887b
joe jiao <yingjunjiao@outlook.com>
2018-04-13 15:13:11 +0800

    init

---
+++ src/Bike.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+/**
+ * 自行车对象
+ * @author joe
+ * @date 2018/4/13
+ */
+public class Bike {
+  /* 品牌 */
+  private String brand;
+  // 售卖标签
+  private String tag;
+  //轮径尺寸
+  private int diameterSize;
+  // 类型
+  private String type;
+  // 速别
+  private int speed;
+  // 车架材质
+  private String frameMaterial;
+  // 轮圈尺寸
+  private int rimesSize;
+  // 受众人群
+  private String crowd;
+  // 上架时间
+  private Date publishTime;
+
+}
